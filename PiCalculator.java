e7cec7a70f0ceb3da0e0bd9383efdb25d5e706e6
Ved Pradhan <vedprad1@gmail.com>
2020-12-12 10:44:32 -0800

    Created PiCalculator.java.

---
+++ PiCalculator.java
@@ -0,0 +1,41 @@
+/**
+ *  Calculates pi using the Law of Conservation of Momentum.
+ *
+ *  @author Ved Pradhan
+ *  @since  December 12, 2020
+ */
+
+public class PiCalculator {
+
+  public static void main(String[] args) {
+    System.out.println(pi(1));
+    System.out.println(pi(2));
+    System.out.println(pi(3));
+    System.out.println(pi(4));
+    System.out.println(pi(5));
+  }
+
+  public static double pi(long d) {
+    double m1 = 1.0;
+    double m2 = Math.pow(100.0, d);
+    double v1 = 0.0;
+    double v2 = -100.0;
+    long collisions = 0;
+
+    while (v2 < v1 || v1 < 0) {
+      if (v1 < 0) {
+        v1 *= -1;
+        collisions++;
+      }
+      else {
+        double vi1 = v1;
+        double vi2 = v2;
+        v1 = ((m1 - m2) * vi1 + 2 * m2 * vi2) / (m1 + m2);
+        v2 = (2 * vi1 + (m2 - m1) * vi2) / (m2 + m1);
+        collisions++;
+      }
+    }
+
+    return collisions / Math.pow(10.0, d);
+  }
+}
