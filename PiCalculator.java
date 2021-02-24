58e79092fc48d973bd2e754db9cde9ef8f23ba5d
Ved Pradhan <vedprad1@gmail.com>
2021-02-24 13:43:08 -0800

    Transferred pi calculating method to Momentum.java

--- PiCalculator.java
+++ PiCalculator.java
@@ -15,27 +15,4 @@ public class PiCalculator {
     System.out.println(pi(5));
   }
 
-  public static double pi(long d) {
-    double m1 = 1.0;
-    double m2 = Math.pow(100.0, d);
-    double v1 = 0.0;
-    double v2 = -100.0;
-    long collisions = 0;
-
-    while (v2 < v1 || v1 < 0) {
-      if (v1 < 0) {
-        v1 *= -1;
-        collisions++;
-      }
-      else {
-        double vi1 = v1;
-        double vi2 = v2;
-        v1 = ((m1 - m2) * vi1 + 2 * m2 * vi2) / (m1 + m2);
-        v2 = (2 * vi1 + (m2 - m1) * vi2) / (m2 + m1);
-        collisions++;
-      }
-    }
-
-    return collisions / Math.pow(10.0, d);
-  }
 }
